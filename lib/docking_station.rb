38979045461f5ddba80c783ef4e33bc88066a941
luciagore <lucy.gore@hotmail.co.uk>
2018-05-17 15:21:15 +0100

    chal 15: SRP

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,28 +4,33 @@ class DockingStation
 
   attr_reader :bikes
 
-  # def bikes 
-  #   @bikes[0] 
+  # def bikes
+  #   @bikes[0]
   # end
 
-  def initialize 
+  def initialize
     @bikes = []
 
   end
 
   def dock(bike)
-    fail 'dock at maximum capacity' if @bikes.length >= 20
+    fail 'dock at maximum capacity' if full?
     @bikes << bike
     # @bikes[0]
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
+  end
 end
-
-
-
-
-
