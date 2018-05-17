8358e23f7424ffa8cfad02f15f943e828125986b
luciagore <lucy.gore@hotmail.co.uk>
2018-05-17 19:08:24 +0100

    updates on challenge 18

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,7 +1,12 @@
 class Bike
 
   def working?
+    # if status == broken ? false : true
     true
   end
+
+  def report_broken
+  end
+
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,8 +2,32 @@ require_relative '../lib/bike'
 
 describe Bike do
 
-  it 'responds to working?' do
-    expect(Bike.new).to respond_to(:working?)
+  describe '#report_broken' do
+    it 'responds to report_broken' do
+      expect(Bike.new).to respond_to(:report_broken)
+    end
+
+    it 'change working to false' do
+      subject.report_broken
+      expect(subject.working?).to be false
+    end
   end
-  
+
+  describe '#working' do
+    it 'responds to working?' do
+      expect(Bike.new).to respond_to(:working?)
+    end
+
+    # it 'takes one argument' do
+    #   expect(Bike.new).to respond_to(:working?).with(1).argument
+    # end
+
+  #   it 'allows user to report bike as broken' do
+  #     bike = Bike.new
+  #     subject.working?("broken")
+  #     expect(subject.working?("broken")).to be false
+  #   end
+
+  end
+
 end
